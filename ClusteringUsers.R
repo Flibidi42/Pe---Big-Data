f40e4110759dfa135ce4c7051425a0c40da91ce3
Philippe.galvan <galvan.philippe@hotmail.fr>
2017-03-13 17:58:55 +0100

    modifs sur la structure des depots2

--- ClusteringUsers.R
+++ ClusteringUsers.R
@@ -3,14 +3,16 @@ Hours <- data.frame(matrix(ncol=12, nrow=0));
 t <- as.POSIXlt(Sys.time());
 Id <- character();
 
-for(i in 1:nrow(JDepot)){
-  Id <- as.character(JDepot[i,"IdUserD"]);
-  if(Id %in% User){
-    t <- as.POSIXlt(strptime(JDepot[i,"DateD"], format = "%FT%H:%M:%S"));
-    Hours[which(Id == User), (t$hour /2) +1] <- Hours[which(Id == User), (t$hour /2) +1] + 1;
-  }
-  else{
-    User <- c(User, Id);
-    Hours <- rbind(Hours, seq(0,0,length.out = 12));
+for(j in 1:length(JDepot)){
+  for(i in j:nrow(JDepot[[j]])){
+    Id <- as.character(JDepot[[j]][i,"IdUserD"]);
+    if(Id %in% User){
+      t <- as.POSIXlt(strptime(JDepot[[j]][i,"DateD"], format = "%FT%H:%M:%S"));
+      Hours[which(Id == User), (t$hour /2) +1] <- Hours[which(Id == User), (t$hour /2) +1] + 1;
+    }
+    else{
+      User <- c(User, Id);
+      Hours <- rbind(Hours, seq(0,0,length.out = 12));
+    }
   }
 }
