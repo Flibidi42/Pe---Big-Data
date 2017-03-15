447c378a73c303459af15e835e0b9f683137ca13
Grégoire RIVIERE <griviere42@gmail.com>
2017-03-15 13:12:10 +0100

    Creating Main Clustering procedure

---
+++ Clustering/MainClustering.R
@@ -0,0 +1,6 @@
+print("Clustering Users");
+source("Clustering/ClusteringUsers.R");
+print("Clustering Users with date (un peu long)");
+source("Clustering/ClusteringUser - with date.R");
+print("Clustering by 4h");
+source("Clustering/ClusteringBy4H.R");
