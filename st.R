e076f79726022027a374e9d4eaf5013d961d2900
ChanutPierre <pierre.chanut@hotmail.fr>
2017-03-16 14:26:43 +0100

    ajout d'instructions dans st_test

--- st.R
+++ st.R
@@ -6,19 +6,25 @@ theDate <- start
 selected_data_temp <- data.frame() 
 selected_data <- data.frame()
 vctr_sum_deposits <- vector()
-vctr_date <- data.frame()
+vctr_date <- vector()
 selected_data_temp <- data.frame()
+
+subset(totaux_depots, as.Date(DateD) == theDate)
 
 #**** selection de tous les depots pour chaque jour de la periode entre les deux dates
 while(theDate <= end){
   vector_boolean <- totaux_depots$DateD == theDate
   vector_boolean
-  vctr_sum_deposits <- c(vctr_sum_deposits,as.numeric(table(vector_boolean)["TRUE"]))
+  #vctr_sum_deposits <- c(vctr_sum_deposits,as.numeric(table(vector_boolean)["TRUE"]))
+  vctr_sum_deposits <- c(vctr_sum_deposits, nrow( subset(totaux_depots, as.Date(DateD) == theDate )))
   vctr_date <- c(vctr_date, theDate)
   print(theDate)
   theDate <- theDate+1
 }
-#v�©rification
+
+date_depots <- data.frame(vctr_date, vctr_sum_deposits)
+date_depots
+#v?©rification
 theDate <- start
 selected_monday <-data.frame()
 vctr_sum_deposits_onmonday <- vector()
