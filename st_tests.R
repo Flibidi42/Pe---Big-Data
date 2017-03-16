54cbb449c6e9bb1d0214dbbcf25473ccf06ecd84
ChanutPierre <pierre.chanut@hotmail.fr>
2017-03-16 11:01:25 +0100

    ajout d'instructions dans st_test

--- st_tests.R
+++ st_tests.R
@@ -1,14 +1,7 @@
 #**** testons !!! 5-410
 vecteur_ma_selection <- vctr_sum_deposits[5:396]
 matrice_selection <- matrix(vecteur_ma_selection, ncol=7)
-
-matrice_selection_2 <- matrix(nrow = nrow(matrice_selection), ncol = ncol(matrice_selection))
-for(i in 1:nrow(matrice_selection)){
-  for(j in 1:ncol(matrice_selection)){
-    matrice_selection_2[i,j]<-matrice_selection[[i,j]]  
-  }
-}
-matrice_selection_2
+matrice_selection
 
 somme = colSums(matrice_selection)
 vecteur_ma_selection
